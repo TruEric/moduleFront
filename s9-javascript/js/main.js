1cbbfb61abd932cc5a22ea1534173c12ca0754e4
Eric Trujillo <eric.trujillo.art@gmail.com>
2020-11-17 22:18:37 -0600

    practicas faltantes

---
+++ s9-javascript/js/main.js
@@ -0,0 +1,5 @@
+let nombreCompleto = prompt('dime el nombre')
+    for(let i = 0; i < nombreCompleto.length; i++){
+        console.log(nombreCompleto.charCodeAt(i).toString())
+        let unicode = nombreCompleto.charCodeAt(i).toString()
+    }
